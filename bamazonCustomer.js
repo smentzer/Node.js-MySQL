e004712e43719a4804e449599f8a6f6b056beab7
Cayde6 <stephaniementzer@gmail.com>
2019-01-29 18:44:05 -0500

    first two prompts work now

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -46,9 +46,11 @@ function afterConnected() {
           + " | "
       );
     }
+  
     dataArr = res;
     //run after display
     itemPurchase();
+  
   });
 
 
@@ -78,6 +80,16 @@ function itemPurchase(){
       }
     }
  ])
+}
+
+
+
+
+
+
+
+
+
 };
 //Once the customer has placed the order, your application should check if your store has enough of the product to meet the customer's request.
 //If not, the app should log a phrase like Insufficient quantity!, and then prevent the order from going through.
@@ -85,3 +97,6 @@ function itemPurchase(){
 //However, if your store does have enough of the product, you should fulfill the customer's order.
 //This means updating the SQL database to reflect the remaining quantity.
 //Once the update goes through, show the customer the total cost of their purchase.
+
+
+
